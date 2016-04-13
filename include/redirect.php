79eed0e1d0b59e751d7c1c3ad240b06e724a6d31
Artem Luchnikov <artem@luchnikov.ru>
2016-04-13 22:25:00 +0300

    #fix Исправлено условие показа мобильной версии для планшетов. Раньше планшетам показывалась мобильная версия, сейчас будет показываться полная.

--- include/redirect.php
+++ include/redirect.php
@@ -27,19 +27,19 @@ if (isMobileVersion() && $_GET['mode'] == 'full') {
 
 $detect = new Mobile_Detect;
 //если пользователь зашел с мобильного устройства и не определена cookie site_version и пользователь находится в полной версии
-if (!isset($_COOKIE['site_version']) && $detect->isMobile() && !isMobileVersion()) {
+if (!isset($_COOKIE['site_version']) && $detect->isMobile() && !$detect->isTablet() && !isMobileVersion()) {
 	setcookie('site_version', 'mobile', 0, '/', 'vsekroham.ru');
 	header('Location: http://m.vsekroham.ru'.$_SERVER['REQUEST_URI']);
 	exit;
 }
 //если пользователь зашел с мобильного устройства и определена cookie site_version как mobile и пользователь находится в полной версии сайта
-elseif (isset($_COOKIE['site_version']) && $_COOKIE['site_version'] == 'mobile' && $detect->isMobile() && !isMobileVersion()) {
+elseif (isset($_COOKIE['site_version']) && $_COOKIE['site_version'] == 'mobile' && $detect->isMobile() && !$detect->isTablet() && !isMobileVersion()) {
 	header('Location: http://m.vsekroham.ru'.$_SERVER['REQUEST_URI']);
 	exit;
 }
 //если пользователь зашел с ПК и пользователь находится в мобильной версии
-/*elseif (!$detect->isMobile() && isMobileVersion()) {
+elseif (!$detect->isMobile() && isMobileVersion()) {
 	setcookie('site_version', 'full', 0, '/', 'vsekroham.ru');
 	header('Location: http://www.vsekroham.ru'.$_SERVER['REQUEST_URI']);
 	exit;
-}*/
+}
