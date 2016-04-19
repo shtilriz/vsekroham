11dca339c848a2f53ae4e06a043b007b42ae20f8
Artem Luchnikov <artem@luchnikov.ru>
2016-04-19 23:08:15 +0300

    Добавлен конфиг сайта

---
+++ bitrix/php_interface/config/common.php
@@ -0,0 +1,13 @@
+<?php
+use Vsekroham\Helpers\EnvironmentHelper;
+/**
+ * Общий инициализатор конфигурации
+ */
+
+EnvironmentHelper::setConfiguration([
+	'catalogIblockId'    => 1,
+	'catalogSkuIblockId' => 2,
+
+	//RetailRocket
+	'rrPartnerId' => '53a000601e994424286fc7d9',
+]);
