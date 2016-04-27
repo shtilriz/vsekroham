36b593caa1e9bb5abf6f71170987cf10e00fcfee
Artem Luchnikov <artem@luchnikov.ru>
2016-04-27 22:23:19 +0300

    #fix Изменен способ вывода пунктов брендов в связи с изменениями результирующего массива в component.php. Task: http://p.natix.ru/public/index.php?path_info=projects/2-vsekrohamru/tasks/795

--- bitrix/templates/main/components/bitrix/menu/brands/template.php
+++ bitrix/templates/main/components/bitrix/menu/brands/template.php
@@ -5,14 +5,16 @@ $this->setFrameMode(true);?>
 <h3>Бренды</h3>
 <ul class="menu__list menu__list_name_brands">
 	<?
-	foreach($arResult as $arItem) {
-		if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
+	foreach($arResult as $key => $arItem) {
+		if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1)
 			continue;
+		if ($key > 11)
+			break;
 		echo '<li class="menu__item"><a class="menu__link'.($arItem["SELECTED"]?' active':'').'" href="'.$arItem["LINK"].'">'.$arItem["TEXT"].'</a></li>';
 	}
 	?>
 	<li class="menu__item">
-		<span class="show-trigger showall">Показать все</span>
+		<a href="/brands/" class="show-trigger showall">Показать все</a>
 	</li>
 </ul>
 <?endif;?>
